bd956af43a7e16033c37fad20975931ec651fdc7
Zhe Deng <42720670+thezhe@users.noreply.github.com>
2024-01-20 22:41:32 +0000

    v1.0.0 draft
    Signed-off-by: Zhe Deng <42720670+thezhe@users.noreply.github.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,12 @@
 # Changelog
 
-## v0.9.0 - 2024/XX/XX
+## v1.0.0 - 2024/01/20
+
+### Fixed
+
+- `CI_BUILD_VERSION` cache variable string breaks without git
+
+## v0.9.0 - 2024/01/14
 
 ### Added
 
--- ci.cmake
+++ ci.cmake
@@ -1,82 +1,61 @@
 #[[
-Builds all CMake targets in `./CMakeLists.txt` in Release mode.
-Also test and package if applicable.
+Build all CMake targets from ./CMakeLists.txt in Release mode.
+Then test and package if applicable.
 Hereinafter, "test" or "package" implies the step exists.
 
     cmake -P <path-to-ci.cmake>
 
 Package Output: build/package
-Cache Variables: CI_BUILD_VERSION (`git describe`).
+Cache Variables: CI_BUILD_VERSION (`git describe`)
 Build Output: build
-Fatal if: Build, test, or package fails.
+Fatal if: build, test, or package fails
 Dependencies: git
 ]]
 
-# See `execute_process` COMMAND_ERROR_IS_FATAL.
+# See `COMMAND_ERROR_IS_FATAL` in `execute_process`
 cmake_minimum_required(VERSION 3.19 FATAL_ERROR)
-
-# Check dependencies.
+# Dependencies
 find_package(Git REQUIRED)
-
-# Variables.
+# Variables
 execute_process(
     COMMAND "${GIT_EXECUTABLE}" describe --always
-    OUTPUT_VARIABLE CI_BUILD_VERSION
-    OUTPUT_STRIP_TRAILING_WHITESPACE COMMAND_ERROR_IS_FATAL ANY
-)
+    OUTPUT_VARIABLE ci_build_version
+    OUTPUT_STRIP_TRAILING_WHITESPACE COMMAND_ERROR_IS_FATAL ANY)
 include(ProcessorCount)
-ProcessorCount(PROCESSOR_COUNT)
-if(NOT PROCESSOR_COUNT)
-    set(PROCESSOR_COUNT 2)
+ProcessorCount(processor_count)
+if(NOT processor_count)
+    set(processor_count 4)
 endif()
-set(BUILD_DIR "build")
-set(BUILD_TYPE "Release")
-set(PACKAGE_DIR "${BUILD_DIR}/package")
-set(PACKAGE_CONFIG "${BUILD_DIR}/CPackConfig.cmake")
-set(PACKAGE_TEMP "${PACKAGE_DIR}/_CPack_Packages")
+set(build_dir "build")
+set(build_type "Release")
+set(package_dir "${build_dir}/package")
+set(package_config "${build_dir}/CPackConfig.cmake")
+set(package_temp "${package_dir}/_CPack_Packages")
 include(CMakePrintHelpers)
-cmake_print_variables(
-    PROCESSOR_COUNT
-    BUILD_DIR
-    BUILD_TYPE
-    PACKAGE_DIR
-    PACKAGE_CONFIG
-    PACKAGE_TEMP
-    CI_BUILD_VERSION
-)
-
-# Required configure.
+cmake_print_variables(processor_count build_dir build_type package_dir
+                      package_config package_temp ci_build_version)
+# Configure
 execute_process(
     COMMAND
-        "${CMAKE_COMMAND}" -B "${BUILD_DIR}"
-        -DCMAKE_BUILD_TYPE:STRING="${BUILD_TYPE}"
-        -DCI_BUILD_VERSION:STRING="${CI_BUILD_VERSION}" COMMAND_ERROR_IS_FATAL
-        ANY
-)
-
-# Required build.
+        "${CMAKE_COMMAND}" -B "${build_dir}"
+        -DCMAKE_BUILD_TYPE:STRING="${build_type}"
+        -DCI_BUILD_VERSION:STRING="${ci_build_version}" COMMAND_ERROR_IS_FATAL
+        ANY)
+# Build
 execute_process(
-    COMMAND "${CMAKE_COMMAND}" --build "${BUILD_DIR}" --config "${BUILD_TYPE}"
-            -j "${PROCESSOR_COUNT}" COMMAND_ERROR_IS_FATAL ANY
-)
-
-# Optional test (fatal if test exists and fails).
+    COMMAND "${CMAKE_COMMAND}" --build "${build_dir}" --config "${build_type}"
+            -j "${processor_count}" COMMAND_ERROR_IS_FATAL ANY)
+# Test
 execute_process(
     COMMAND
-        "${CMAKE_CTEST_COMMAND}" -C "${BUILD_TYPE}" -j "${PROCESSOR_COUNT}"
-        --output-on-failure --test-dir "${BUILD_DIR}" COMMAND_ERROR_IS_FATAL
-        ANY
-)
-
-#[[ Optional package (fatal if CPackConfig.cmake exists and packaging fails or
- temp files are not removable). ]]
+        "${CMAKE_CTEST_COMMAND}" -C "${build_type}" -j "${processor_count}"
+        --output-on-failure --test-dir "${build_dir}" COMMAND_ERROR_IS_FATAL
+        ANY)
+# Package (fatal if temp files are not removeable)
 execute_process(
     COMMAND
         "${CMAKE_CPACK_COMMAND}" -DCPACK_VERBATIM_VARIABLES:BOOL=TRUE
-        -DCPACK_STRIP_FILES:BOOL=TRUE -B "${PACKAGE_DIR}" --config
-        "${PACKAGE_CONFIG}" -C "${BUILD_TYPE}"
-)
-execute_process(
-    COMMAND "${CMAKE_COMMAND}" -E rm -rf -- "${PACKAGE_TEMP}"
-            COMMAND_ERROR_IS_FATAL ANY
-)
+        -DCPACK_STRIP_FILES:BOOL=TRUE -B "${package_dir}" --config
+        "${package_config}" -C "${build_type}")
+execute_process(COMMAND "${CMAKE_COMMAND}" -E rm -rf -- "${package_temp}"
+                        COMMAND_ERROR_IS_FATAL ANY)
